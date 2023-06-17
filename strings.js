65020c8e187f22e45cb4291fbc957676b1101077
Gathoni Wanjira <janetgathoni910@gmail.com>
2023-06-17 20:35:51 +0300

    String interpolation

---
+++ strings.js
@@ -0,0 +1,9 @@
+// Make a function that will return a greeting statement that uses an input;
+// Your program should return, "Hello, <name> how are you doing today?".
+
+
+function greet(name){
+    //your code here
+    console.log(`Hello, ${name} how are you doing today?`)
+  }
+greet("ryan");
